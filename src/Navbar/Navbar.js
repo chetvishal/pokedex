38b566441ab50c3778a351dd5bec65afe441532b
chetvishal <vishal1982c@gmail.com>
2020-11-15 13:11:48 +0530

    created Navbar.js component

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navbar() {
+
+    return (
+        <nav>
+            <div class="nav-wrapper">
+                <a href="#" class="brand-logo">Pokedex</a>
+               
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
